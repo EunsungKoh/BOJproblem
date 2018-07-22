32c42be3e1044a494f14eb9cb2743c8b7f5fb972
eunsung <gldghd5662@gmail.com>
2018-07-22 15:26:12 +0900

    README 수정 및 본선문제 시작

--- 제1회 천하제일 코딩대회 본선/14650.cpp
+++ 제1회 천하제일 코딩대회 본선/14650.cpp
@@ -1,1 +1,14 @@
 //걷다보니 신천역 삼 (Small)
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int menu,temp;
+int main() {
+
+  scanf("%d" , &menu );
+  for(int i =0;i<1;i++){
+    scanf("%d" , &temp );
+  }
+  printf("%d",temp);
+  return 0;
+}
