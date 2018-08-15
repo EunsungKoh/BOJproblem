ea882774ea4e85a91cb1e8785bf64fdc72aacb5f
eunsung <gldghd5662@gmail.com>
2018-08-16 00:13:09 +0900

    테스트케이스에서는 다 통과

--- 대학생 기본반 - trytty/2667.cpp
+++ 대학생 기본반 - trytty/2667.cpp
@@ -13,10 +13,9 @@ vector < pair < int,int > > v;
 void gogo(int x,int y){
   visit[x][y]=true;
   howmany[danji]++;
-  cout<<"("<<x<<","<<y<<")"<<endl;
-  if (x<N&&!visit[x+1][y]&&apart[x+1][y]) gogo(x+1,y);
+  if (x<N-1&&!visit[x+1][y]&&apart[x+1][y]) gogo(x+1,y);
   if (x>0&&!visit[x-1][y]&&apart[x-1][y]) gogo(x-1,y);
-  if (y<N&&!visit[x][y+1]&&apart[x][y+1]) gogo(x,y+1);
+  if (y<N-1&&!visit[x][y+1]&&apart[x][y+1]) gogo(x,y+1);
   if (y>0&&!visit[x][y-1]&&apart[x][y-1]) gogo(x,y-1);
 }
 int main (){
@@ -30,13 +29,13 @@ int main (){
   }
   for( auto it = v.begin();it !=v.end(); it++){
     if(!visit[it->first][it->second]){
-      cout<<"gogo start"<<endl;
       gogo(it->first,it->second);
       danji++;
     }
   }
   sort(howmany,howmany+danji);
   cout<< danji<<endl;
+  if (danji==0) cout<<0<<endl;
   for (int i=0;i<danji;i++){  
     cout<<howmany[i]<<endl;
   }
