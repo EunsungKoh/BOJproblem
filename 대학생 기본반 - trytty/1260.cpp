c0ca911f77d5daba01e0db32f91c2b28518cdcbb
eunsung <gldghd5662@gmail.com>
2018-08-20 23:15:35 +0900

    dfs 다시 공부

--- 대학생 기본반 - trytty/1260.cpp
+++ 대학생 기본반 - trytty/1260.cpp
@@ -4,7 +4,16 @@
 #include <algorithm>
 using namespace std;
 bool edge[1001][1001]={false};
+bool visit[1001]={false};
 int N,M,start,temp1,temp2;
+void dfs(int now){
+  visit[now]=true;
+  printf("%d 번째 노드 방문\n",now+1);
+  for(int i =0; i <N;i++){
+    if(!visit[i]&&edge[now][i])
+      dfs(i);
+  }
+}
 int main (){
   cin >>N>>M>>start;
   for (int i=1;i<=M;i++){
@@ -12,10 +21,9 @@ int main (){
     edge[temp1][temp2]=true;
     edge[temp2][temp1]=true;
   }
-  for (int i=1;i<=M;i++){
-    for (int j=1;j<=M;j++){
-      if(edge[i][j]) printf("(%d,%d)\n",i,j);
-    }
+
+  for (int i=0;i<N;i++){
+    dfs(i);
   }
 
   return 0;
